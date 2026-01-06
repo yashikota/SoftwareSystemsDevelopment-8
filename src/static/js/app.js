501ef59f314f39e00bfe1caf44303eb00dfaced6
yashikota <52403688+yashikota@users.noreply.github.com>
2026-01-06 23:28:00 +0900

    docker

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -4,6 +4,7 @@ function App() {
         <Container>
             <Row>
                 <Col md={{ offset: 3, span: 6 }}>
+                    <h1 className="text-center my-4">yashikota</h1>
                     <TodoListCard />
                 </Col>
             </Row>
